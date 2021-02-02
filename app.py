6192961926db5cc997b7bb4adae97e2e47d6c5f4
Mohamed EL-Khouly <msirag95@gmail.com>
2021-02-02 14:57:41 +0200

    search venu

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import func
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -190,16 +190,20 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+  search_term = request.form.get('search_term')
+  venues = Venue.query.filter(func.lower(Venue.name).like('%'+search_term.lower()+"%")).all()
+  data = []
+  
+  for venue in venues:
+    print((venue.name))
+    data.append({
+      "id": venue.id,
+      "name": venue.name,
+      "num_upcoming_shows": len(venue.coming_shows()),
+    })
+  response ={
+    'count': len(data),
+    'data' : data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
